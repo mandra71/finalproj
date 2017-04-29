ef01d1ed558708bd008c24d9fb180be372838e5f
Michael Andrade <mandra71@gmail.com>
2017-04-29 16:58:07 +0000

    dynamic hello world with timestamp

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the views index.")
+    return HttpResponse("Hello, world. You're at the views index. It is currently " + time.strftime("%c"))
 # Create your views here.
