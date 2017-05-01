de7c505eda7b495f6a5bbcbce0ca864d1adb64f6
Michael Andrade <mandra71@gmail.com>
2017-05-01 02:06:22 +0000

    Reformatted the css designs and added a chart that doesn't function due to free trial limitations

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,55 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 
 from .models import Choice, Question
+import json
 
+# Common base class for FC
+class FusionCharts:
 
+   #constructorOptions = {}
+   constructorTemplate = """
+     <script type="text/javascript">
+         FusionCharts.ready(function () {
+             new FusionCharts(__constructorOptions__);
+         });
+     </script>"""
+   renderTemplate = """
+     <script type="text/javascript">
+         FusionCharts.ready(function () {
+             FusionCharts("__chartId__").render();
+         });
+     </script>
+   """
+   # constructor
+   def __init__(self, type, id, width, height, renderAt, dataFormat, dataSource): 
+      self.constructorOptions = {}  
+      self.constructorOptions['type'] = type
+      self.constructorOptions['id'] = id
+      self.constructorOptions['width'] = width
+      self.constructorOptions['height'] = height
+      self.constructorOptions['renderAt'] = renderAt
+      self.constructorOptions['dataFormat'] = dataFormat
+      #dataSource = unicode(dataSource, errors='replace')
+      self.constructorOptions['dataSource'] = dataSource
+   # render the chart created
+   # It prints a script and calls the FusionCharts javascript render method of created chart   
+   def render(self):
+    self.readyJson = json.dumps(self.constructorOptions)
+    self.readyJson = FusionCharts.constructorTemplate.replace('__constructorOptions__', self.readyJson)
+    self.readyJson = self.readyJson + FusionCharts.renderTemplate.replace('__chartId__', self.constructorOptions['id'])
+    self.readyJson = self.readyJson.replace('\\n', '')
+    self.readyJson = self.readyJson.replace('\\t', '')
+
+    if(self.constructorOptions['dataFormat'] == 'json'):
+      self.readyJson = self.readyJson.replace('\\', '')
+      self.readyJson = self.readyJson.replace('"{', "{")
+      self.readyJson = self.readyJson.replace('}"', "}")
+      
+    return self.readyJson
+   
+   
+   
+    
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -20,10 +67,24 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    dataSource = {}
+    dataSource['chart'] = { 
+		"caption": "Poll Results",
+        "xAxisName": "Choice",
+        "yAxisName": "Votes",
+        "theme": "fint"
+    }
+    dataSource['data'] = []
+    for choice in question.choice_set.all():
+        data = {}
+        data['label'] = choice.choice_text
+        data['value'] = choice.votes
+        dataSource['data'].append(data)
+    column2D = FusionCharts("column2D", "ex1", "600", "400", "chart-1", "json", dataSource)
+    return render(request, 'polls/results.html', {'output': column2D.render(), 'question': question})
+        	
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
